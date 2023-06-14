aec4a2e48c6433369cd03fe57fcf82f3e428d9b6
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-14 11:56:20 +0300

    Update usersSlice.js

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -8,13 +8,32 @@ const initialState = {
   error: undefined,
 };
 
-const fetchUsers = createAsyncThunk()
+const URL = 'https://jsonplaceholder.typicode.com/users'
+const fetchUsers = createAsyncThunk('users/fetchUsers', async(thunkAPI) => {
+    try{
+        const response = await axios(URL);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  extraReducers: {
-
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+    })
+    .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+    })
+    .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+    })
   },
 });
 
