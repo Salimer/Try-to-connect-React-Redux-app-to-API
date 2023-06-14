efc8c278d194924dcb3eae997c74e6d66281eb1a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-14 11:44:07 +0300

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
-const User = (user) => (
+import PropTypes from 'prop-types';
+
+const User = ({ user }) => (
   <li>
-    <h1>user.name</h1>
+    <h1>{user.name}</h1>
   </li>
 );
+
+User.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default User;
