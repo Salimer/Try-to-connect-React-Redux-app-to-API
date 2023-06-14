71cb1d537d9a2dc4d087a389658535dc2635e8c7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-14 11:57:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import { Provider } from 'react-redux';
-import logo from './logo.svg';
 import './App.css';
 import store from './store/store';
+import ListUsers from './components/ListUsers';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-            and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ListUsers/>
     </Provider>
   );
 }
