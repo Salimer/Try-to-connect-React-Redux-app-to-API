18a25e64dab7413bd6f6236e3d5977d9d516d123
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-14 10:52:46 +0300

    Create usersSlice.js

---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    users: [],
+    isLoading: true,
+    error: undefined,
+}
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    extraReducers: {
+        
+    }
+})
